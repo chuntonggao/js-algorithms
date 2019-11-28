f8ac2f3df54e4b7113babaf49331b6d36c50e95b
Chuntong Gao <heihegao@gmail.com>
2019-11-27 22:21:51 -0800

    selection sort

--- fundamentals/algorithms/sorting.ts
+++ fundamentals/algorithms/sorting.ts
@@ -1,11 +1,15 @@
 const log = console.log;
 
+const swap = (arr: number[], idx1: number, idx2: number): void => {
+    [ arr[idx1], arr[idx2] ] = [ arr[idx2], arr[idx1] ];
+};
+
 const bubbleSort = (arr: number[]): number[] => {
     for (let i = arr.length; i > 0; i--) {
         let noSwaps: boolean = true;
         for (let j = 0; j < i - 1; j++) {
             if (arr[j + 1] < arr[j]) {
-                [ arr[j], arr[j + 1] ] = [ arr[j + 1], arr[j] ];
+                swap(arr, j, j + 1);
                 noSwaps = false;
             }
         }
@@ -14,4 +18,21 @@ const bubbleSort = (arr: number[]): number[] => {
     return arr;
 };
 
+const selectionSort = (arr: number[]): number[] => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) min = j;
+        }
+        if (min !== i) swap(arr, i, min);
+    }
+    return arr;
+};
+
+log('===== BUBBLE SORT =====');
 log(bubbleSort([ 8, 1, 2, 3, 4, 5, 6, 7 ]));
+log('===== =========== =====');
+
+log('===== SELECTION SORT =====');
+log(selectionSort([ 8, 1, 2, 3, 4, 5, 6, 7 ]));
+log('===== =========== =====');
