3b8c3d7c0e154c6dd2da16e75f1948fd859df040
Chuntong Gao <heihegao@gmail.com>
2019-11-30 09:20:36 -0800

    refacotr

--- leetcode/146LRUCache.java
+++ leetcode/146LRUCache.java
@@ -1,93 +1,112 @@
 import java.util.*;
 
-class LRUCache {
-    class DLinkedNode {
-        int key;
-        int value;
-        DLinkedNode prev;
-        DLinkedNode next;
-    }
+class Solution {
+    class Node {
+        private int key;
+        private int value;
+        private Node prev;
+        private Node next;
 
-    private void addNode(DLinkedNode node) {
-        // always add ned node right after head
-        node.prev = head;
-        node.next = head.next;
-        head.next.prev = node;
-        head.next = node;
-    }
+        public Node() {}
 
-    private void removeNode(DLinkedNode node) {
-        DLinkedNode prev = node.prev;
-        DLinkedNode next = node.next;
-        prev.next = next;
-        next.prev = prev;
-    }
-
-    private void moveToHead(DLinkedNode node) {
-        removeNode(node);
-        addNode(node);
-    }
-
-    private DLinkedNode popTail() {
-        DLinkedNode res = tail.prev;
-        removeNode(res);
-        return res;
-    }
-
-    private Map<Integer, DLinkedNode> cache = new HashMap<>();
-    private int size;
-    private int capacity;
-    private DLinkedNode head, tail;
-
-    public LRUCache(int capacity) {
-        this.size = 0;
-        this.capacity = capacity;
-        head = new DLinkedNode();
-        tail = new DLinkedNode();
-        head.next = tail;
-        tail.prev = head;
-    }
-
-    public int get(int key) {
-        DLinkedNode node = cache.get(key);
-        if (node == null)
-            return -1;
-        moveToHead(node);
-        return node.value;
-    }
-
-    public void put(int key, int value) {
-        DLinkedNode node = cache.get(key);
-        if (node == null) {
-            DLinkedNode newNode = new DLinkedNode();
-            newNode.key = key;
-            newNode.value = value;
-            cache.put(key, newNode);
-            addNode(newNode);
-            size++;
-            if (size > capacity) {
-                DLinkedNode tail = popTail();
-                cache.remove(tail.key);
-                size--;
-            }
-        } else {
-            node.value = value;
-            moveToHead(node);
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
         }
     }
 
+    class DLinkedList {
+        private Node head;
+        private Node tail;
+
+        public DLinkedList() {
+            head = new Node();
+            tail = new Node();
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        public void addNode(Node node) {
+            // always add ned node right after head
+            node.prev = head;
+            node.next = head.next;
+            head.next.prev = node;
+            head.next = node;
+        }
+
+        public void removeNode(Node node) {
+            Node prev = node.prev;
+            Node next = node.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+
+        public void moveToHead(Node node) {
+            removeNode(node);
+            addNode(node);
+        }
+
+        public Node popTail() {
+            Node res = tail.prev;
+            removeNode(res);
+            return res;
+        }
+    }
+
+    class LRUCache {
+        private Map<Integer, Node> cache = new HashMap<>();
+        private DLinkedList list = new DLinkedList();
+        private int size = 0;
+        private int capacity;
+
+        public LRUCache(int capacity) {
+            this.size = 0;
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            Node node = cache.get(key);
+            if (node == null)
+                return -1;
+            list.moveToHead(node);
+            return node.value;
+        }
+
+        public void put(int key, int value) {
+            Node node = cache.get(key);
+            if (node == null) {
+                Node newNode = new Node(key, value);
+                cache.put(key, newNode);
+                list.addNode(newNode);
+                size++;
+                if (size > capacity) {
+                    Node tail = list.popTail();
+                    cache.remove(tail.key);
+                    size--;
+                }
+            } else {
+                node.value = value;
+                list.moveToHead(node);
+            }
+        }
+    }
+
+    private LRUCache cache = new LRUCache(2 /* capacity */ C);
     public static void main(String[] args) {
-        LRUCache cache = new LRUCache(2 /* capacity */ );
         cache.put(1, 1);
         cache.put(2, 2);
         System.out.println(cache.get(1)); // returns 1
-        cache.put(3, 3);                  // evicts key 2
+        cache.put(3, 3); // evicts key 2
         System.out.println(cache.get(2)); // returns -1 (not found)
-        cache.put(4, 4);                  // evicts key 1
+        cache.put(4, 4); // evicts key 1
         System.out.println(cache.get(1)); // returns -1 (not found)
         System.out.println(cache.get(3)); // returns 3
         System.out.println(cache.get(4)); // returns 4
     }
+
+
 }
 /**
  * Your LRUCache object will be instantiated and called as such: LRUCache obj =
